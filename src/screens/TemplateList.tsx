e4798181f8b32a33deba9f9fb856a1db725e9d56
tami0304 <hiyokotaro.1101@gmail.com>
2022-09-30 18:45:24 +0900

    [change] 移植

---
+++ src/screens/TemplateList.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import WorkSpaceFrame from '../components/WorkSpaceFrame';
+
+export default function  TemplateList() {
+  const cards = ['1','2','3','4'];
+  return (
+    <ScrollView>
+      <WorkSpaceFrame name="いえ" cards={cards} />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
