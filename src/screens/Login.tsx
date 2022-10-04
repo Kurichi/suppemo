921fc9f9cf581e813c9c90d3b5946f8a517adcc2
tami0304 <hiyokotaro.1101@gmail.com>
2022-10-04 21:51:02 +0900

    vscode config change

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -2,61 +2,61 @@ import { Button } from '@rneui/base';
 import React from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function  Login() {
-    return(
-        <View style={styles.container} >
-            <View>
-                <TextInput value="ID" style={styles.loginForm} />
-                <TextInput value="Password" style={styles.loginForm} />
-            </View>
-            <View style={styles.loginContainer}>
-                <View style={styles.loginButton}>
-                    <Text style={styles.loginButtonText}>Login</Text>
-                </View>
-                <Text style={styles.signupMessage}>サインアップはこちら</Text>
-            </View>
+export default function Login() {
+  return (
+    <View style={styles.container} >
+      <View>
+        <TextInput value="ID" style={styles.loginForm} />
+        <TextInput value="Password" style={styles.loginForm} />
+      </View>
+      <View style={styles.loginContainer}>
+        <View style={styles.loginButton}>
+          <Text style={styles.loginButtonText}>Login</Text>
         </View>
-    );
+        <Text style={styles.signupMessage}>サインアップはこちら</Text>
+      </View>
+    </View>
+  );
 
-    
+
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFF8B0',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    loginForm: {
-        fontSize: 32,
-        color: '#C1C1C1',
-        width: 316,
-        paddingTop: 60,
-        borderBottomWidth: 1,
-    },
-    loginButton: {
-        width: 212,
-        height: 67,
-        backgroundColor: '#FCD12C',
-        borderRadius: 15,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    loginButtonText: {
-        fontSize: 32,
-        color: 'rgba(0,0,0,0.4)',
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF8B0',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginForm: {
+    fontSize: 32,
+    color: '#C1C1C1',
+    width: 316,
+    paddingTop: 60,
+    borderBottomWidth: 1,
+  },
+  loginButton: {
+    width: 212,
+    height: 67,
+    backgroundColor: '#FCD12C',
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginButtonText: {
+    fontSize: 32,
+    color: 'rgba(0,0,0,0.4)',
 
 
-    },
-    signupMessage: {
-        fontSize: 13,
-        color: '#787777',
-        paddingTop: 16,
-    },
-    loginContainer: {
-        alignItems: 'center',
-        paddingTop: 60,
-    },
+  },
+  signupMessage: {
+    fontSize: 13,
+    color: '#787777',
+    paddingTop: 16,
+  },
+  loginContainer: {
+    alignItems: 'center',
+    paddingTop: 60,
+  },
 
 });
