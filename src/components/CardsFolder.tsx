abee913500c18d594c8fa4d559ba915c760b5fa6
Yammer2 <111174613+Yammer2@users.noreply.github.com>
2022-10-06 23:53:31 +0900

    [change] CardFolder and add expo-camera package by yammer

--- src/components/CardsFolder.tsx
+++ src/components/CardsFolder.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { ScrollView } from 'react-native-gesture-handler';
 
-export default function  CardsFolder() {
+
+interface folder_type {
+  id: number,
+  feather_name: string,
+  background_color: string,
+}
+
+export default function CardsFolder() {
+  const folders: folder_type[] = [
+    { id: 0, feather_name: "star-o", background_color: '#8BD8A5' },
+    { id: 1, feather_name: "smile-o", background_color: '#8BD805' }
+  ];
+
+
   return (
     <View style={styles.cardsFolder}>
-      <View style={styles.tag}>
-        <Feather name="star" size={24} color="white" />
-      </View>
+      <ScrollView
+        horizontal={true}
+        contentContainerStyle={{ flexGrow: 1 }}
+        style={styles.scrollBar}>
+        {folders.map(data => {
+          return (
+            <View style={[styles.tag, { backgroundColor: data.background_color }]}>
+              <Icon name={data.feather_name} size={48} color='white' />
+            </View>
+          );
+        })}
+      </ScrollView>
       <View style={styles.folderFlame}>
         <View style={styles.folder}>
           <Text>Helllllllllllllllllllllo!</Text>
@@ -30,6 +55,8 @@ const styles = StyleSheet.create({
     width: 90,
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   folderFlame: {
     backgroundColor: '#8BD8A5',
@@ -44,5 +71,9 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     paddingVertical: 8,
     width: '94%',
+  },
+  scrollBar: {
+    backgroundColor: 'blue',
+
   }
 });
