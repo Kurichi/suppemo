060311bdd582f20a3833ce3d887b05e9b8dd65c8
Kurichi <yuuyapp080@gmail.com>
2022-12-22 23:06:24 +0900

    [add] friend endpoint

--- backend/golang/app/handler/friend.go
+++ backend/golang/app/handler/friend.go
@@ -3,21 +3,53 @@ package handler
 import (
 	"net/http"
 	middlware "suppemo-api/middleware"
+	"suppemo-api/model"
 
 	"github.com/labstack/echo/v4"
 )
 
+type requestBody struct {
+	UID string `json:"uid" query:"uid" form:"uid"`
+}
+
 func AddFriend(c echo.Context) error {
 	authHeader := c.Request().Header.Get("Authorization")
-	_, err := middlware.Auth(authHeader)
+	user, err := middlware.Auth(authHeader)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+		return c.JSON(http.StatusBadRequest, err.Error())
 	}
 
-	return nil
+	reqBody := new(requestBody)
+	if err := c.Bind(reqBody); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	if user.UID == reqBody.UID {
+		return c.String(http.StatusBadRequest, "you can't add friend yourself")
+	}
+
+	if err := model.CreateFriend(user.UID, reqBody.UID); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	return c.String(http.StatusOK, "add friend complete")
 }
 
 func GetFriends(c echo.Context) error {
+	authHeader := c.Request().Header.Get("Authorization")
+	user, err := middlware.Auth(authHeader)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
 
-	return nil
+	friends, err := model.FindFriends(user.UID)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	type response struct {
+		FriendUID []string `json:"friend_uid"`
+	}
+
+	return c.JSON(http.StatusOK, &response{friends})
 }
